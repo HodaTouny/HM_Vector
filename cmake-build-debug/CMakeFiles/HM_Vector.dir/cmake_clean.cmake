911644df55cb7fde8bed89e3e46cb1cd373c6dff
mayahesham <96448594+mayahesham@users.noreply.github.com>
2022-12-07 11:35:36 +0200

    add functions

---
+++ cmake-build-debug/CMakeFiles/HM_Vector.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HM_Vector.dir/main.cpp.obj"
+  "CMakeFiles/HM_Vector.dir/main.cpp.obj.d"
+  "CMakeFiles/HM_Vector.dir/vectorlib.cpp.obj"
+  "CMakeFiles/HM_Vector.dir/vectorlib.cpp.obj.d"
+  "HM_Vector.exe"
+  "HM_Vector.exe.manifest"
+  "HM_Vector.pdb"
+  "libHM_Vector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HM_Vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
